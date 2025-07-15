98ad34b9ef9e8b1a9eb46e40d56900c9e26d1f8b
DXDDAGGER <sreeharipradeep38@gmail.com>
2025-07-15 15:48:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -10,14 +10,14 @@ from Script import script
 id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
-SESSION = environ.get('SESSION', 'TechVJBot')
+SESSION = environ.get('SESSION', 'EmmaMyersBot')
 API_ID = int(environ.get('API_ID', '6674102'))
 API_HASH = environ.get('API_HASH', '1ca8fedb9d767db9f162054fc42bc4b4')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split()
+PICS = (environ.get('PICS', 'https://envs.sh/ifA.jpg')).split()
 
 
 # Admins & Users
